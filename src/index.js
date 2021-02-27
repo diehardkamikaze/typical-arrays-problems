803c7f41a868a8ae55179a3e91501fdf425a2cd5
diehardkamikaze <diehardkamikaze@gmail.com>
2021-02-27 22:06:32 +0300

    init: typical-array-problems implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+	var min;
+	var i;
+	
+	if (arguments.length == 0 || array.length == 0)
+		return (0);
+	min = +Infinity;
+	i = 0;
+	while (i < array.length)
+	{
+		if (array[i] < min)
+			min = array[i];
+		i++;
+	}
+  return (min);
 }
 
 exports.max = function max (array) {
-  return 0;
+	var max;
+	var i;
+	
+	if (arguments.length == 0 || array.length == 0)
+		return (0);
+	max = -Infinity;
+	i = 0;
+	while (i < array.length)
+	{
+		if (array[i] > max)
+			max = array[i];
+		i++;
+	}
+  return (max);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	var result;
+	var i;
+	
+	if (arguments.length == 0 || array.length == 0)
+		return (0);
+	i = 0;
+	result = 0;
+	while (i < array.length)
+		result += array[i++];
+  return (result/array.length);
 }
